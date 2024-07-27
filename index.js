5f90fa99e00e7e00132d4cf09343c617258ff399
phadatul <phadatul@gmail.com>
2024-07-27 17:11:06 +0530

    Added server started message

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ var http=require("http")
 http.createServer(function(request,response){
     var data=new Date();
     response.end("Date is:"+data.toLocaleString());
-}).listen(8282);
+}).listen(8282,function(error){
+  if (error) throw error
+  console.log("Serer successfully started........")
+});
